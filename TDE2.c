c6c0859ae2386627b30c1efe5878f5c57d3e8160
Eric Salvi <ericsalvi67@gmail.com>
2025-03-20 17:09:51 -0300

    Create TDE2.c
    TDE 2

---
+++ TDE2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct lib_name{
+    char name[20];
+    struct lib_name *prox;
+}NAMES;
+
+NAMES * insert_sorted(NAMES **actual, char *value)
+{
+    NAMES *new, *aux = *actual, *prev = NULL;
+    
+    new = (NAMES*) malloc(sizeof(NAMES));
+    
+    strcpy(new->name,value);
+    
+    while (aux != NULL && strcmp(aux->name,value))
+    {
+        prev = aux;
+        aux = aux->prox;
+    }
+    if (prev == NULL)
+    {
+        new->prox = *actual;
+        *actual = new;
+    }
+    else
+    {
+        new->prox = prev->prox;
+        prev->prox = new;
+    }
+    
+    return *actual;
+}
+
+
+void show_list(NAMES *actual)
+{
+    NAMES *aux = actual;
+    while (aux != NULL)
+    {
+        printf("%s\n",aux->name);
+        aux = aux->prox;
+    }
+}
+
+
+int main()
+{
+    NAMES *actual = NULL;
+    
+    actual = insert_sorted(&actual,"Joao");
+    actual = insert_sorted(&actual,"Paulo");
+    show_list(actual);
+    
+    return  0;
+}
