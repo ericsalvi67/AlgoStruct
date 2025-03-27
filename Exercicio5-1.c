d70098158e4797a126034906c739f807feb1a39a
Éric Salvi <ericsalvi67@hotmail.com>
2025-03-27 19:25:05 -0300

    🎉 adiciona implementação da lista circular e função de inserção

---
+++ Exercicio5-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista {
+ int valor;
+ struct Lista *prox;
+}Lista;
+
+void escreveListas(Lista *inicio){
+
+   Lista *temp = inicio;
+   do 
+   {
+       printf("endereco: %p valor: %d proximo: %p\n\n\n", temp, temp->valor, temp->prox);
+       temp = temp->prox;
+   }while(temp != inicio);
+
+   printf("\n\n");
+}
+
+void insereEricSalvi(Lista **inicio, int valor)
+{
+    Lista *novo = (Lista*) malloc(sizeof(Lista));
+
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    if((*inicio) == NULL)
+    {
+        *inicio = novo;
+        (*inicio)->prox = novo;
+        return;
+    }
+ 
+    Lista *temp = *inicio;
+
+    while(temp->prox != *inicio)
+    {
+        temp = temp->prox;
+    }
+
+    temp->prox = novo;
+    novo->prox = *inicio;
+
+}
+
+int main()
+{
+    Lista *inicio = NULL;
+    insereEricSalvi(&inicio,1);
+    insereEricSalvi(&inicio,2);
+    insereEricSalvi(&inicio,3);
+    escreveListas(inicio);
+
+    free(inicio);
+    return 0;
+}
